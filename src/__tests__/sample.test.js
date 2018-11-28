6a58597f3ab12690a71801c6eb1ba07e7f2c956b
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2018-11-29 03:37:17 +0900

    Use jsdom

--- src/__tests__/sample.test.js
+++ src/__tests__/sample.test.js
@@ -3,3 +3,8 @@ import { testUtil } from '../utils'
 test('test util', () => {
   expect(testUtil()).toEqual('test util')
 })
+
+test('use jsdom in this test file', () => {
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
+});
